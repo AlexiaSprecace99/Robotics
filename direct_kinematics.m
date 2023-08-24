ce8a9111edfa07bc6fc1cf122029e803d038d0d0
Alexia Sprecacè <91467761+AlexiaSprecace99@users.noreply.github.com>
2023-08-24 10:16:14 +0200

    Republish_all

--- direct_kinematics.m
+++ direct_kinematics.m
@@ -8,10 +8,16 @@ function [p0, p1, p2, p3] = direct_kinematics(q)
     a2 = 1;
     a3 = 1;
 
-    p0 = [0;0;0];
-    p1 = [a1*cos(q1); a1*sin(q1);0];
-    p2 = p1+[a2*cos(q2);a2*sin(q2);0];
-    %p2 = p1+[a2*cos(q2); a2*sin(q2);0];
-    p3 = [(a2 + a3)*(cos(q1)*cos(q2) - sin(q1)*sin(q2)) + a1*cos(q1);(a2 + a3)*(cos(q1)*sin(q2) + cos(q2)*sin(q1)) + a1*sin(q1);0];
+%     p0 = [0;0;0];
+%     p1 = [a1*cos(q1); a1*sin(q1);0];
+%     p2 = p1+[a2*cos(q2);a2*sin(q2);0];
+%     %p2 = p1+[a2*cos(q2); a2*sin(q2);0];
+%     p3 = [(a2 + a3)*(cos(q1)*cos(q2) - sin(q1)*sin(q2)) + a1*cos(q1);(a2 + a3)*(cos(q1)*sin(q2) + cos(q2)*sin(q1)) + a1*sin(q1);0];
+
+p0 = [0;0;0];
+p1 = [a1*cos(q1); a1*sin(q1);0];
+p2 = p1+[a2*cos(q1+q2);a2*sin(q1+q2);0];
+p3 = [a1*cos(q1) + a3*cos(q3)*(cos(q1)*cos(q2) - sin(q1)*sin(q2)) + a2*cos(q1)*cos(q2) - a2*sin(q1)*sin(q2); a1*sin(q1) + a3*cos(q3)*(cos(q1)*sin(q2) + cos(q2)*sin(q1)) + a2*cos(q1)*sin(q2) + a2*cos(q2)*sin(q1);-a3*sin(q3)];
+
                                                                                     
 end
